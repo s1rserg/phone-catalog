62c78b09634c931d149e9a1aef8a1d4158e2c498
sirserg <sirserg05@gmail.com>
2025-06-16 21:14:17 +0300

    feat: move to clean repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Footer } from './modules/shared/components/footer';
+import { Header } from './modules/shared/components/header';
+import { useEffect } from 'react';
 import './App.scss';
+import { getAllProducts } from './api/products';
+import { productsSlice } from './store/slices/products';
+import { useAppDispatch, useAppSelector } from './hooks/hooks';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => {
+  const dispatch = useAppDispatch();
+  const product = useAppSelector(state => state.products);
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const products = await getAllProducts().then(response => response);
 
-export const App: React.FC = () => {
+        dispatch(productsSlice.actions.setProducts(products));
+      } catch (error) {}
+    };
+
+    fetchProducts();
+  }, [dispatch]);
+
+  if (!product.length) {
+    return null;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="App">
+      <Header />
+      <main className="container">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
